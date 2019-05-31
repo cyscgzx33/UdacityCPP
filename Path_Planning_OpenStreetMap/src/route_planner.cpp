f1e613d545f122a2bb73379e42653929a46dc754
cyscgzx33 <chenyus@umich.edu>
2019-05-31 12:03:08 -0400

    feat: added RoutePlanner::ConstructFinalPath() method

--- Path_Planning_OpenStreetMap/src/route_planner.cpp
+++ Path_Planning_OpenStreetMap/src/route_planner.cpp
@@ -1,6 +1,40 @@
 #include "route_planner.h"
 #include <algorithm>
 
-RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
+RoutePlanner::RoutePlanner(RouteModel& model, 
+                           float start_x, 
+                           float start_y, 
+                           float end_x, 
+                           float end_y)
+                           : m_Model(model) 
+{   
+    // Convert inputs to percentage:
+    // Since the nodes store their coordinates between 0-1, 
+    // and user is supposed to input the floats from 0-100
+    start_x *= 0.01;
+    start_y *= 0.01;
+    end_x *= 0.01;
+    end_y *= 0.01;
 
+    start_node = &m_Model.FindClosestNode(start_x, start_y);
+    end_node = &m_Model.FindClosestNode(end_x, end_y);
+}
+
+
+std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node* current_node) {
+
+    std::vector<RouteModel::Node> path_found;
+    // keep looping until it finds the start node, whose parent is nullptr
+    RouteModel::Node* curNode = current_node;
+    while (!curNode) {
+        path_found.push_back(*curNode);
+        // Also keep track of the total path distance
+        if (!curNode->parent) this->distance += curNode->distance(*curNode->parent); 
+        curNode = curNode->parent;
+    }
+
+    // Scale by multiplying by the model's scale
+    this->distance *= m_Model.MetricScale();
+
+    return path_found;
 }
--- Path_Planning_OpenStreetMap/src/route_planner.h
+++ Path_Planning_OpenStreetMap/src/route_planner.h
@@ -8,10 +8,16 @@
 
 class RoutePlanner {
   public:
-    RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
+    RoutePlanner(RouteModel& model, float start_x, float start_y, float end_x, float end_y);
     // Add public variables or methods declarations here.
+    float GetDistance() { return distance; }
 
   private:
     // Add private variables or methods declarations here.
     RouteModel &m_Model;
+    RouteModel::Node* start_node;
+    RouteModel::Node* end_node;
+    float distance = 0.0;
+
+    std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node*);
 };
