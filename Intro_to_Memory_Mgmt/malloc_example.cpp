e359a47e41b90103861ae59e41379611f5867c87
cyscgzx33 <chenyus@umich.edu>
2019-08-03 11:51:39 -0400

    feat: refinded malloc_example.cpp by adding comparison with memset

--- Intro_to_Memory_Mgmt/malloc_example.cpp
+++ Intro_to_Memory_Mgmt/malloc_example.cpp
@@ -1,16 +1,29 @@
-#include <iostream>
-// Included library for memset
-#include <cstring>
-// included library for malloc
+/* [description of Malloc()]
+ * Malloc is a function which is essentially forerunner to new operator in C++, originally inherited from C. 
+ * Its function is to allocate a block of specific size, measured in bytes of memory, 
+ * where result of this operation (function) is pointer to the beginning of the block. 
+ * Data stored in this memory block is not initialized, which means that this space has non determinate values.
+ * 
+ * Prototype of this function is:
+ * void* malloc(size_t size);
+ * Size is unsigned integer value which represent memory block in bytes. 
+ * It is important to note that in case of failed allocation return value is null pointer.
+ * */
+
+#include <iostream> // Included library for memset
+#include <cstring> // included library for malloc
 #include <cstdlib>
+
 int main() {
     int *ptr = NULL;
     // Notice how pointer is allocated to single int memory space 
     // but later is redicerted to show to memory block
     ptr = new(std::nothrow) int;
-    //  We can initialize memory block using malloc
-    //  declaring memory space for array of 4 integers
-    ptr = (int*)std::malloc(4*sizeof(int));
+    // We can initialize memory block using malloc
+    // declaring memory space for: array of 4 integers
+    // [Note] we need type cast here, as the function prototype if "void*"
+    // otherwise: compiler will throw that "error: invalid conversion from ‘void*’ to ‘int*’ [-fpermissive]""
+    ptr = std::malloc(4*sizeof(int));
         for (int i=0; i<4; i++){
         std::cout << "Address: "
         << &ptr[i] << " Value: "<< ptr[i] << std::endl;
