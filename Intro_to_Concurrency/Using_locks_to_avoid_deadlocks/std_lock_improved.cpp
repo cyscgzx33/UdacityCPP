12b85ec17c5b97da04fed6e1e8504f1650c5ff00
Yusen Chen <chenyus@umich.edu>
2019-09-05 17:11:42 -0400

    feat: modified some comments on std_lock_improved.cpp

--- Intro_to_Concurrency/Using_locks_to_avoid_deadlocks/std_lock_improved.cpp
+++ Intro_to_Concurrency/Using_locks_to_avoid_deadlocks/std_lock_improved.cpp
@@ -1,13 +1,13 @@
 /**
  * [Description]
  * 
- * In most cases, your code should only hold one lock on a mutex at a time. Occasionally you can nest your locks, 
- * for example by calling a subsystem that protects its internal data with a mutex while holding a lock on another mutex, but it is generally better to avoid locks on multiple mutexes at the same time, if possible. Sometimes, however, 
- * it is necessary to hold a lock on more than one mutex because you need to perform an operation on two different data elements, each protected by its own mutex.
- * In the unique_lock.cpp, we have seen that using several mutexes at once can lead to a deadlock, if the order of locking them is not carefully managed. 
- * To avoid this problem, the system must be told that both mutexes should be locked at the same time, so that one of the threads takes over both 
- * locks and blocking is avoided. That's what the std::lock() function is for - you provide a set of lock_guard or unique_lock objects and 
- * the system ensures that they are all locked when the function returns.
+ * In the following deadlock-free code, std::lock is used to ensure that the mutexes are always 
+ * locked in the same order, regardless of the order of the arguments. 
+ * Note that std::adopt_lock option allows us to use std::lock_guard on an already locked mutex.
+ * 
+ * As a rule of thumb, programmers should try to avoid using several mutexes at once. 
+ * Practice shows that this can be achieved in the majority of cases. 
+ * For the remaining cases though, using std::lock is a safe way to avoid a deadlock situation.
  * 
  */
 
